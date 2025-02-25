a89a5f4849bafad9af4aaa7ceae0d101e7ea5398
Nicolás <jumpjam12@gmail.com>
2025-02-24 23:33:30 -0500

    Requerimientos para el proyecto final y creación del método main

---
+++ src/main/java/com/ejercicios/poo/final_lenguaje_de_programacion_main/Final_Lenguaje_de_programacion_main.java
@@ -0,0 +1,20 @@
+/**
+ *  Definir el tema a trabajar (supermercado, vivero, farmacia, disquera)
+ * 
+ * construir una registradora de ventas:
+ * 
+ * 1.  Debe simular un inventario del que se descuentan productos comprados.
+ * 2. Se consulta la información de estos y se realizan procesos de totalización de una compra.
+ * 3. Consulta de información general y específica sobre un producto.
+ * 4. Cargar nuevos productos o actualizar los existentes.
+ * 
+ * Recuerde que, al utilizar la programación funcional, cada elemento solicitado debe consistir en funciones, 
+ * además recuerde que la programación funcional involucra el uso de recursión para tareas repetitivas evitando en su totalidad el uso de ciclos.
+ * 
+*/
+public class Final_Lenguaje_de_programacion_main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}

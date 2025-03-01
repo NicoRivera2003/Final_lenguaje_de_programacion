f0df9d621184dcd6a9ed8e0258e4f0077755778e
Nicolás <jumpjam12@gmail.com>
2025-03-01 13:06:33 -0500

    Actualización de requisitos del programa del supermercado para el proyecto

--- src/main/java/Main/Final_Lenguaje_de_programacion_main.java
+++ src/main/java/Main/Final_Lenguaje_de_programacion_main.java
@@ -4,10 +4,17 @@ package Main;
  * 
  * construir una registradora de ventas:
  * 
- * 1. Debe simular un inventario del que se descuentan productos comprados.
- * 2. Se consulta la información de estos y se realizan procesos de totalización de una compra.
- * 3. Consulta de información general y específica sobre un producto.
- * 4. Cargar nuevos productos o actualizar los existentes.
+   1. Registrar ventas (código, id, fecha).
+   2. Simular un inventario del que se descuentan productos comprados.
+   3. Consultar información de los registros.
+   4. Mostrar el valor total de la compra.
+   5. Consultar de información general (cantidad) y específica sobre un producto (Nombre, id, precio y descripción).
+   6. Cargar nuevos productos.
+   7. Actualizar productos existentes.
+   8. Mostrar el promedio de ventas por semana, mes y año.
+   9. Mostrar el producto más vendido con nombre y precio.
+   10.Mostrar el producto menos vendido con nombre y precio.
+   11.Mostrar los productos sin stock, cantidad y nombre.
  * 
  * Recuerde que, al utilizar la programación funcional, cada elemento solicitado debe consistir en funciones, 
  * además recuerde que la programación funcional involucra el uso de recursión para tareas repetitivas evitando en su totalidad el uso de ciclos.
@@ -17,6 +24,9 @@ public class Final_Lenguaje_de_programacion_main {
 
     public static void main(String[] args) {
         System.out.println("Bienvenido a la registradora de ventas");
+        System.out.println(" ");
+        System.out.println("Opciones: ");
+        System.out.println("1. Comprar \n2. Inventario \n3. Consultar información de producto \n4. Cargar nuevo producto");
         
     }
 }
